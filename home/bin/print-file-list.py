bbf6ce606867d73957a1c4954ec362e9f43ef4dd
coder <coder@code.xlan.top>
2021-04-12 20:52:44 +0800

    add Wildcard

--- home/bin/print-file-list.py
+++ home/bin/print-file-list.py
@@ -6,19 +6,23 @@ from pathlib import Path
 
 
 def init():
-    if len(sys.argv)-1!=1:
-        eprint('One path is required')
+    if not len(sys.argv)-1 in (1,2):
+        eprint('Invalid number of arguments')
         sys.exit(1)
     tdir = Path(sys.argv[1])
     if not tdir.is_dir():
         eprint(f'No such directory:{tdir}')
         sys.exit(2)
 
-def print_files(path:Path,err:bool):
+def print_files(path:Path,err:bool,wildcard:str=None):
     try:
-        for f in path.iterdir():
+        if wildcard:
+            d=path.rglob(wildcard)
+        else:
+            d=path.iterdir()
+        for f in d:
             if f.is_dir():
-                print_files(f,err)
+                print_files(f,err,wildcard)
             else:
                 print(f)
     except Exception as e:
@@ -26,10 +30,17 @@ def print_files(path:Path,err:bool):
         err=True
 
 
-
-if __name__ == '__main__':
+def main():
     err=False
     tdir = Path(sys.argv[1])
-    print_files(tdir,err)
+    if len(sys.argv)-1==2:
+        wc=sys.argv[2]
+    else:
+        wc=None
+    print_files(tdir,err,wc)
+    print(tdir,err,wc)
     if err:
         sys.exit(3)
+
+if __name__ == '__main__':
+    main()
